a6154041c5016199093e175129bcfa7fd34fe4d9
nguyenvietanh221112005 <AnhNV.B23CC013@stu.ptit.edu.vn>
2025-10-21 16:43:48 +0700

    up

---
+++ demo/src/main/java/com/example/demo/Service/SongService.java
@@ -0,0 +1,34 @@
+package com.example.demo.Service;
+
+import com.example.demo.Dao.SongRepository;
+import com.example.demo.Model.Song;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class SongService {
+
+  @Autowired
+  private SongRepository songRepository;
+
+  public List<Song> getTopSongs() {
+    List<Song> danhSach = songRepository.findAllByLuotThichDesc();
+
+    if (danhSach.isEmpty()) {
+      return songRepository.findAllRandom();
+    }
+
+    return danhSach;
+  }
+
+
+  public SongService(SongRepository songRepository) {
+    this.songRepository = songRepository;
+  }
+
+  public List<Song> getSongsByCategory(Integer category) {
+    return songRepository.findByCategoryId(category);
+  }
+}
